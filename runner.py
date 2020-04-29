b7225b69d199547710d1d8efbe29da7cba81b938
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-29 19:10:19 +0800

    runner test related added
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- runner.py
+++ runner.py
@@ -1,6 +1,19 @@
+import pdb
+
 from python_logging_rabbitmq import RabbitMQHandler
 
 import utils
+
+
+class AMQPURL:
+    # host = "termite.rmq.cloudamqp.com"  # (Load balanced)
+    # passwd = "QrBHPPxbsd8IuIxKrCnX3-RGoLKaFhYI"
+    # username = "drdsfaew"
+    # Vhost = "drdsfaew"
+    host = "127.0.0.1"  # (Load balanced)
+    passwd = "guest"
+    username = "guest"
+    Vhost = "/"
 
 
 class Runner:  # blade runner
@@ -8,7 +21,7 @@ class Runner:  # blade runner
     to check the running status)
     """
 
-    def __init__(self, kernel_code="TestKernel", AMQPURL="", **kwargs):
+    def __init__(self, kernel_code="TestKernel", AMQPURL=None, **kwargs):
         self.kernel_name = kernel_code
         self.AMQPURL = AMQPURL
 
@@ -21,8 +34,21 @@ class Runner:  # blade runner
         """
 
         logger = utils.get_logger()
-        rabbit = RabbitMQHandler(host="localhost", port=5672)
-        # create kernel and run
+        rabbit = RabbitMQHandler(
+            host=self.AMQPURL.host,
+            port=5672,
+            username=self.AMQPURL.username,
+            password=self.AMQPURL.passwd,
+            exchange="logs_topic",
+            connection_params={"virtual_host": self.AMQPURL.Vhost},
+            declare_exchange=True,
+        )
+        # rabbit.connection_params["virtual_host"] = self.AMQPURL.Vhost create
+        # kernel and run
         logger.addHandler(rabbit)
+
+        pdb.set_trace()
         logger.debug("test")
+        logger.debug("more test")
+
         # kernel.set_logger(self.kernel_name, handler=rabbit)
