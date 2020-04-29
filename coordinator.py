fe3bf18d69c7e628932ef62a5925dbdfebdbba93
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-29 16:39:28 +0800

    TDD: add coordinator initial test cases
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- coordinator.py
+++ coordinator.py
@@ -1,8 +1,8 @@
 class Coordinator:
     """run in controller side, the runners run in dockers with GPUs"""
 
-    def __init__(self):
-        pass
+    def __init__(self, tmp_path):
+        self.tmp_path = tmp_path
 
     def add_runner(self):
         """add_runner will create kernel folders and change the code"""
@@ -20,4 +20,10 @@ class Coordinator:
         """use the message queue, just use this right after push, listen for
         result, debug local first"""
         "use RE change source code to add the log collector"
+        pass
+
+    def create_runner(self, config):
+        """
+        config will be size and model right now
+        """
         pass
