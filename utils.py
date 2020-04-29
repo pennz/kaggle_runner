679a8e459557c86b6647cb2b84070990689533b6
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-30 00:06:36 +0800

    Fix amqp url
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- utils.py
+++ utils.py
@@ -28,24 +28,33 @@ from tqdm import tqdm
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-class AMQPURL:
+class AMQPURL_DEV:
     host = "termite.rmq.cloudamqp.com"  # (Load balanced)
     passwd = "QrBHPPxbsd8IuIxKrCnX3-RGoLKaFhYI"
     username = "drdsfaew"
     Vhost = "drdsfaew"
-    # host = "127.0.0.1"  # (Load balanced)
-    # passwd = "guest"
-    # username = "guest"
+    # host = "127.0.0.1"  # (Load balanced) passwd = "guest" username = "guest"
     # Vhost = "/"
 
-    def __init__(self, host, passwd, Vhost, username):
+
+class AMQPURL:
+    def __init__(
+        self,
+        host=AMQPURL_DEV.host,
+        passwd=AMQPURL_DEV.passwd,
+        Vhost=AMQPURL_DEV.Vhost,
+        username=AMQPURL_DEV.username,
+    ):
         self.host = host
         self.passwd = passwd
         self.Vhost = Vhost
         self.username = username
 
     def string(self):
-        return f"amqp://{self.username}:{self.passwd}@{self.host}{self.Vhost}"
+        Vhost = self.Vhost
+        if self.Vhost == "/":
+            Vhost = ""
+        return f"amqp://{self.username}:{self.passwd}@{self.host}/{Vhost}"
 
 
 BIN_FOLDER = (
