52ac50ce32829573db08eb3211cb81d3bd50402c
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-29 08:35:45 +0800

    Local mq test fine
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- runner.py
+++ runner.py
@@ -1,13 +1,16 @@
 from python_logging_rabbitmq import RabbitMQHandler
 
+import utils
+
 
 class Runner:  # blade runner
     """Runner should run in docker container, and then a controller(or just mq
     to check the running status)
     """
 
-    def __init__(self, kernel_code="TestKernel"):
+    def __init__(self, kernel_code="TestKernel", AMQPURL="", **kwargs):
         self.kernel_name = kernel_code
+        self.AMQPURL = AMQPURL
 
     def _attach_data_collector(self, kernel):
         """
@@ -17,6 +20,9 @@ class Runner:  # blade runner
 
         """
 
+        logger = utils.get_logger()
         rabbit = RabbitMQHandler(host="localhost", port=5672)
         # create kernel and run
-        kernel.set_logger(self.kernel_name, handler=rabbit)
+        logger.addHandler(rabbit)
+        logger.debug("test")
+        # kernel.set_logger(self.kernel_name, handler=rabbit)
