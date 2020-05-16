c4dd06d2d2d16aaeef8f9b1af8a638e311979123
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-16 16:24:59 +0800

    Just encode utf-8
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/utils/visualizer.py
+++ kaggle_runner/utils/visualizer.py
@@ -24,7 +24,8 @@ def visualize_model_preds(model,val_data, x_valid, y_valid, indices=[0, 17, 1, 2
             color = f'{Fore.RED}'
             symbol = '\u2716'
 
-        print('{}{} {}'.format(color, str(idx+1) + ". " + label, symbol))
+        print('{}{} {}'.format(color, str(idx+1) + ". " + label,
+                               symbol.encode('utf-8')))
         print(f'{Style.RESET_ALL}')
         print("ORIGINAL")
         print(comments[idx]); print("")
