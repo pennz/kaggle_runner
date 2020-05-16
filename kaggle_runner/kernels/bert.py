feddc1b8d702af0cc7ae58208edbe29685942df5
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-16 15:32:12 +0800

    Move codes to test script

--- kaggle_runner/kernels/bert.py
+++ kaggle_runner/kernels/bert.py
@@ -23,6 +23,7 @@ calls = ReduceLROnPlateauLogCBs((x_valid, y_valid))
 EPOCHS = 2
 
 # ### Define the model
+model_distilbert = None
 
 def build_distilbert_model(transformer, max_len=512):
     input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
@@ -49,26 +50,3 @@ with strategy.scope():
     from_pretrained('distilbert-base-multilingual-cased')
     model_distilbert = build_distilbert_model(transformer_layer, max_len=512)
 
-model_distilbert.summary()
-# -
-
-# ### Train the model
-
-train_history = model_distilbert.fit(
-    train_dataset,
-    steps_per_epoch=N_STEPS,
-    validation_data=valid_dataset,
-    callbacks = calls,
-    epochs=EPOCHS
-)
-
-# ### Visualize model architecture
-
-# SVG(tf.keras.utils.model_to_dot(model_distilbert, drpi=70).create(prog='dot', format='svg'))
-
-# ### Visualize model predictions
-
-# + {"_kg_hide-input": true}
-# def visualize_model_preds(model,val_data, x_valid, y_valid, indices=[0, 17, 1, 24]):
-visualize_model_preds(model_distilbert, val_data, x_valid, y_valid)
-# -
