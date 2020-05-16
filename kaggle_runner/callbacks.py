44feda32fb29378359cd86bc04639529e577657f
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-16 19:43:57 +0800

    Added model checkpoint cb
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/callbacks.py
+++ kaggle_runner/callbacks.py
@@ -4,6 +4,7 @@ from tensorflow.keras.callbacks import (Callback, CSVLogger, ModelCheckpoint,
 
 # ### Define ROC-AUC evaluation metric
 
+
 class RocAucEvaluation(Callback):
     def __init__(self, validation_data=(), interval=1):
         super(Callback, self).__init__()
@@ -15,21 +16,24 @@ class RocAucEvaluation(Callback):
         if epoch % self.interval == 0:
             y_pred = self.model.predict(self.X_val, verbose=0)
             score = roc_auc_score(self.y_val, y_pred)
-            print("\n ROC-AUC - epoch: {:d} - score: {:.6f}".format(epoch+1, score))
+            print(
+                "\n ROC-AUC - epoch: {:d} - score: {:.6f}".format(epoch+1, score))
 
 
 def ReduceLROnPlateauLogCBs(validation_data):
     cb = []
 
     reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss',
-                                    factor=0.3, patience=3,
-                                    verbose=1, mode='auto',
-                                    epsilon=0.0001, cooldown=1, min_lr=0.000001)
+                                       factor=0.3, patience=3,
+                                       verbose=1, mode='auto',
+                                       epsilon=0.0001, cooldown=1, min_lr=0.000001)
     cb.append(reduceLROnPlat)
     log = CSVLogger('log.csv')
     cb.append(log)
 
     RocAuc = RocAucEvaluation(validation_data=validation_data, interval=1)
     cb.append(RocAuc)
+    ckpt = ModelCheckpoint("toxic.hdf5", save_best_only=True, verbose=1)
+    cb.append(ckpt)
 
     return cb
