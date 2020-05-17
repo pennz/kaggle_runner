fdcdf7ee7fc0bb1a61955350955481671b0c5061
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-17 21:02:36 +0800

    Still loger thing
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/utils/__init__.py
+++ kaggle_runner/utils/__init__.py
@@ -1,5 +1,3 @@
-from .utils import AMQPURL, get_logger
+from .utils import AMQPURL, get_logger, logger
 
-__all__ = ["utils",'logger','AMQPURL']
-
-logger = get_logger()
+__all__ = ["utils",'logger','AMQPURL', 'logger']
--- kaggle_runner/utils/tpu.py
+++ kaggle_runner/utils/tpu.py
@@ -1,13 +1,19 @@
 import tensorflow as tf
 
 from kaggle_datasets import KaggleDatasets
+from kaggle_runner.utils import utils
 
 AUTO = tf.data.experimental.AUTOTUNE
 
-tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
-tf.config.experimental_connect_to_cluster(tpu)
-tf.tpu.experimental.initialize_tpu_system(tpu)
-strategy = tf.distribute.experimental.TPUStrategy(tpu)
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except ValueError as e:
+    utils.logger.error(e)
+    tpu = None
+    strategy = None
 
 GCS_DS_PATH = KaggleDatasets().get_gcs_path('jigsaw-multilingual-toxic-comment-classification')
 
