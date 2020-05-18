3af1c329eebf9689178b827a9bd0e0ba350cef14
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-18 17:24:06 +0800

    Improve pdb
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/__init__.py
+++ kaggle_runner/__init__.py
@@ -2,15 +2,19 @@ __all__ = ["runners.runner", "runners.coordinator", "utils"]
 
 import ripdb
 
+import ipdb
+
 from .defaults import RIPDB
 from .utils import logger
 
 __port = 4444
+__rip = False
 
 def may_debug():
     global __port
+    global __rip
 
-    if RIPDB:
+    if RIPDB and not __rip:
         try:
             logger.debug(f"try port %s for ripdb", __port)
             ripdb.set_trace(port=__port)
@@ -18,7 +22,9 @@ def may_debug():
             __port += 1
             logger.debug(f"try port %s for ripdb", __port)
             ripdb.set_trace(port=__port)
-        else:
-            __port += 1
         finally:
-            logger.debug(f"port %s for ripdb done", __port-1)
+            __port += 1
+            __rip = True
+            logger.debug(f"port %s for ripdb finally", __port-1)
+    else:
+        ipdb.set_trace()
