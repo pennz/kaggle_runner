f8c9be99a6ce6ffdee22195b8a89ccf2d6f28921
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-20 07:26:50 +0800

    just try debug
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/kernels/bert.py
+++ kaggle_runner/kernels/bert.py
@@ -41,8 +41,10 @@ def _build_distilbert_model_adv(transformer, max_len=512):
     return _build_distilbert_model(transformer, max_len=512)
 
 def _build_distilbert_model(transformer, max_len=512):
-    may_debug()
-    from kaggle_runner import may_debug
+    try:
+        may_debug()
+    except Exception:
+        pass
     input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
     sequence_output = transformer(input_word_ids)[0]
 
