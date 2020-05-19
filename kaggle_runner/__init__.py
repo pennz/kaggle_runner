fa74d7b80860b7e51b205f6e4f99db93a585d9c4
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-20 07:25:20 +0800

    may_debug import problem? remove ripdb
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/__init__.py
+++ kaggle_runner/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["logger", "runners.runner", "runners.coordinator", "utils"]
+__all__ = ["may_debug","logger", "runners.runner", "runners.coordinator", "utils"]
 
 import ipdb
 
@@ -7,27 +7,6 @@ from .utils import logger
 
 logger = logger
 
-__port = 4444
-__rip = False
-
-
 def may_debug():
-    global __port
-    global __rip
-
-    # if RIPDB and not __rip:
-    #     try:
-    #         logger.debug(f"try port %s for ripdb", __port)
-    #         ripdb.set_trace(port=__port)
-    #     except OSError:
-    #         __port += 1
-    #         logger.debug(f"try port %s for ripdb", __port)
-    #         ripdb.set_trace(port=__port)
-    #     finally:
-    #         __port += 1
-    #         __rip = True
-    #         logger.debug(f"port %s for ripdb finally", __port-1)
-    # else:
-
     if DEBUG:
         ipdb.set_trace()
