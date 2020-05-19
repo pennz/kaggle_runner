8c7acd85e0d8bc93316b769172453574714b25ce
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-19 20:30:30 +0800

    y label no need sizedown
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/datasets/bert.py
+++ kaggle_runner/datasets/bert.py
@@ -122,10 +122,11 @@ TRAIN_LEN = len(x_train)
 
 if DEBUG:
     x_train = x_train[:TRAIN_LEN//10, :140]
-    y_train = y_train[:TRAIN_LEN//10, :140]
+    y_train = y_train[:TRAIN_LEN//10]  # y just all pass, they are labels
     x_valid = x_valid[:, :140]
-    y_valid = y_valid[:, :140]
+    # y_valid = y_valid[:] # it just one dimention
     x_test  = x_test[:, :140]
+
     TRAIN_LEN = TRAIN_LEN//10
 
 # +
