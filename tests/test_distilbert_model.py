19d5149713a5c4c7b5fca78b01b0e4be1cbbbd44
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-20 17:12:36 +0800

    testcase just call cls_setup function to get model
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- tests/test_distilbert_model.py
+++ tests/test_distilbert_model.py
@@ -40,8 +40,7 @@ class Test_distilbert_model:
         assert train_dataset is not None
 
     def test_summary(self):
-        self.model_distilbert = build_distilbert_model_singleton(140)
-
+        self.setup_class()
         self.model_distilbert.summary()
         assert True
 
