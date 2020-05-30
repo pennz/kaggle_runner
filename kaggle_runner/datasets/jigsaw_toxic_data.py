646d7b7524c3390c5f6cac054023e59343e1284a
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-30 11:26:57 +0800

    bert: data prepare print info
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/datasets/jigsaw_toxic_data.py
+++ kaggle_runner/datasets/jigsaw_toxic_data.py
@@ -20,7 +20,7 @@ def get_toxic_comment(p, col_name="comment_text"):
     dtr = get_column(os.path.join(DATA_PATH, p), col_name=col_name)
 
     r = random.randint(0, len(dtr)-1)
-    logger.debug("toxic comment data example: %s", dtr[r])
+    logger.debug("toxic data example %s.[%s]:\n %s\n", p, col_name, dtr[r])
 
     return dtr
 
@@ -30,7 +30,7 @@ def merge_all_data():
     td = get_toxic_comment(TD, "content")
     c = pd.concat([dtr, vd, td])
     c.to_csv(OUT_PATH, index=False)
-    print("%d %d %d" % (len(dtr), len(vd), len(td)))
+    print("%d %d %d" % (len(dtr), len(vd), len(td))) # lines info: 223549 8000 63812
 
 if __name__ == "__main__":
     merge_all_data()
