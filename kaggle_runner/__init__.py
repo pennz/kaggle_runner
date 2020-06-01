3522ef33cb1da3fefd1601c298a00f3cce596738
Vincent Zhou <fireflysuccess@gmail.com>
2020-06-01 18:13:28 +0800

    pdb just put to may_debug"
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- kaggle_runner/__init__.py
+++ kaggle_runner/__init__.py
@@ -2,11 +2,11 @@ __all__ = ["may_debug","logger", "runners.runner", "runners.coordinator", "utils
 
 from .defaults import DEBUG, RIPDB
 from .utils import logger
-import pdb
 
 logger = logger
 
 def may_debug(force=False):
+    import pdb
     if force:
         pdb.set_trace()
     else:
