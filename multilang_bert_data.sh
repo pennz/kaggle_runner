4ff281eaf4ae0fecc32234d13e1040c13077090f
Vincent Zhou <fireflysuccess@gmail.com>
2020-06-03 07:15:00 +0800

    stage detail
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- multilang_bert_data.sh
+++ multilang_bert_data.sh
@@ -3,99 +3,101 @@ INPUT=/kaggle/input/jigsaw-multilingula-toxicity-token-encoded
 BATCH_SIZE=32
 
 if [ ! -d $INPUT/XNLI ]; then
-    # prepare data for kaggle dataset/google storage
-    curl -O https://dl.fbaipublicfiles.com/XNLI/XNLI-1.0.zip
-    curl -O https://dl.fbaipublicfiles.com/XNLI/XNLI-MT-1.0.zip
-    curl -O https://storage.googleapis.com/bert_models/2018_11_23/multi_cased_L-12_H-768_A-12.zip
+	# prepare data for kaggle dataset/google storage
+	curl -O https://dl.fbaipublicfiles.com/XNLI/XNLI-1.0.zip
+	curl -O https://dl.fbaipublicfiles.com/XNLI/XNLI-MT-1.0.zip
+	curl -O https://storage.googleapis.com/bert_models/2018_11_23/multi_cased_L-12_H-768_A-12.zip
 
-    extrace_to_folder() {
-        zip_name=$1
-        zn=${zip_name//.zip/}
-        #mkdir "$zn" && pushd "$zn" && unzip $OLDPWD/"$zip_name" && popd
-        unzip -d "$zn" "$zip_name"
-    }
-    export -f extrace_to_folder
+	extrace_to_folder() {
+		zip_name=$1
+		zn=${zip_name//.zip/}
+		#mkdir "$zn" && pushd "$zn" && unzip $OLDPWD/"$zip_name" && popd
+		unzip -d "$zn" "$zip_name"
+	}
+	export -f extrace_to_folder
 
-    find . -name "*.zip" -print0 | xargs -0 -I{} bash -xc 'extrace_to_folder {}'
+	find . -name "*.zip" -print0 | xargs -0 -I{} bash -xc 'extrace_to_folder {}'
 
-    DEV_TEST=XNLI-1.0
-    MT_TRAIN=XNLI-MT-1.0
+	DEV_TEST=XNLI-1.0
+	MT_TRAIN=XNLI-MT-1.0
 
-    mkdir XNLI && mv $DEV_TEST/* XNLI && mv $MT_TRAIN/* XNLI
-    rmdir "$DEV_TEST"
-    rmdir "$MT_TRAIN"
+	mkdir XNLI && mv $DEV_TEST/* XNLI && mv $MT_TRAIN/* XNLI
+	rmdir "$DEV_TEST"
+	rmdir "$MT_TRAIN"
 
-    export BERT_BASE_DIR=$PWD/multi_cased_L-12_H-768_A-12 # or multilingual_L-12_H-768_A-12
-    export XNLI_DIR=$PWD/XNLI/XNLI-MT-1.0/
+	export BERT_BASE_DIR=$PWD/multi_cased_L-12_H-768_A-12 # or multilingual_L-12_H-768_A-12
+	export XNLI_DIR=$PWD/XNLI/XNLI-MT-1.0/
 else
-    if [ ! -z $TPU_NAME ]; then
-        GCS_M_DS_PATH=$(python -c 'from kaggle_runner.utils.tpu import GCS_M_DS_PATH; print(GCS_M_DS_PATH)')
-        BATCH_SIZE=32*8
-    fi
-    if [ ! -z $GCS_M_DS_PATH ]; then
-        INPUT=$GCS_M_DS_PATH
-    fi
-    export BERT_BASE_DIR=$INPUT/multi_cased_L-12_H-768_A-12 # or multilingual_L-12_H-768_A-12
-    export XNLI_DIR=$INPUT/XNLI/XNLI-MT-1.0
+	if [ ! -z $TPU_NAME ]; then
+		GCS_M_DS_PATH=$(python -c 'from kaggle_runner.utils.tpu import GCS_M_DS_PATH; print(GCS_M_DS_PATH)')
+		BATCH_SIZE=32*8
+	fi
+	if [ ! -z $GCS_M_DS_PATH ]; then
+		INPUT=$GCS_M_DS_PATH
+	fi
+	export BERT_BASE_DIR=$INPUT/multi_cased_L-12_H-768_A-12 # or multilingual_L-12_H-768_A-12
+	export XNLI_DIR=$INPUT/XNLI/XNLI-MT-1.0
 fi
 
 #test our datasets
 
-STAGE="extract_feature"
-STAGE_Detail="pickle"
-export STAGE_Detail
+STAGE=${STAGE:-"extract_feature"}
+STAGE_DETAIL=${STAGE_DETAIL:-"pickle"}
+export STAGE
+export STAGE_DETAIL
 
 if [ $STAGE = "extract_feature" ]; then
 
-    TI=/tmp/input.txt
-    if [ ! -f $TI ]; then
-        python kaggle_runner/datasets/jigsaw_toxic_data.py
-        sed -i 's/"\{1,3\}\s*\(.*\)\s*"\{1,3\}$/\1/' $TI
-        wc_l_info=$(wc -l $TI)
-    fi
+	TI=/tmp/input.txt
+	if [ ! -f $TI ]; then
+		python kaggle_runner/datasets/jigsaw_toxic_data.py
+		sed -i 's/"\{1,3\}\s*\(.*\)\s*"\{1,3\}$/\1/' $TI
+		wc_l_info=$(wc -l $TI)
+	fi
 
-    echo "head of the comments list:"
-    head /tmp/input.txt
-    echo "lines info: $wc_l_info"
+	echo "head of the comments list:"
+	head /tmp/input.txt
+	echo "lines info: $wc_l_info"
 
-    git clone https://github.com/pennz/bert
-    cd bert || exit 1
+	git clone https://github.com/pennz/bert
+	cd bert || exit 1
 
-    export STORAGE_BUCKET=gs://kaggle_runner
-    export TASK_NAME=toxic
+	export STORAGE_BUCKET=gs://kaggle_runner
+	export TASK_NAME=toxic
 
-    if [ ! -z $TPU_NAME ]; then
-        #export BERT_BASE_DIR=gs://cloud-tpu-checkpoints/bert/uncased_L-12_H-768_A-12
-        #export BERT_BASE_DIR=gs://cloud-tpu-checkpoints/bert/multi_cased_L-12_H-768_A-12
-        TPU_Parameter="--use_tpu=True --tpu_name=$TPU_NAME --output_dir=${STORAGE_BUCKET}/${TASK_NAME}"
-        OUT_PARA="--output_file=${STORAGE_BUCKET}/${TASK_NAME}/multi_cased_features.jsonl"
-    else
-        #export BERT_BASE_DIR=gs://cloud-tpu-checkpoints/bert/multi_cased_L-12_H-768_A-12
-        OUT_PARA="--output_file=/home/multi_cased_features.jsonl"
-    fi
+	if [ ! -z $TPU_NAME ]; then
+		#export BERT_BASE_DIR=gs://cloud-tpu-checkpoints/bert/uncased_L-12_H-768_A-12
+		#export BERT_BASE_DIR=gs://cloud-tpu-checkpoints/bert/multi_cased_L-12_H-768_A-12
+		TPU_Parameter="--use_tpu=True --tpu_name=$TPU_NAME --output_dir=${STORAGE_BUCKET}/${TASK_NAME}"
+		OUT_PARA="--output_file=${STORAGE_BUCKET}/${TASK_NAME}/multi_cased_features.jsonl"
+	else
+		#export BERT_BASE_DIR=gs://cloud-tpu-checkpoints/bert/multi_cased_L-12_H-768_A-12
+		OUT_PARA="--output_file=/home/multi_cased_features.jsonl"
+	fi
 
-    python extract_features.py \
-        --input_file=/tmp/input.txt \
-        --vocab_file="$BERT_BASE_DIR/vocab.txt" \
-        --bert_config_file="$BERT_BASE_DIR/bert_config.json" \
-        --init_checkpoint="$BERT_BASE_DIR/bert_model.ckpt" \
-        --layers=-1,-2,-3,-4 \
-        --max_seq_length=128 $OUT_PARA $TPU_Parameter \
-        --batch_size=$BATCH_SIZE
+	python extract_features.py \
+		--input_file=/tmp/input.txt \
+		--vocab_file="$BERT_BASE_DIR/vocab.txt" \
+		--bert_config_file="$BERT_BASE_DIR/bert_config.json" \
+		--init_checkpoint="$BERT_BASE_DIR/bert_model.ckpt" \
+		--layers=-1,-2,-3,-4 \
+		--stage_detail=$STAGE_DETAIL \
+		--max_seq_length=128 $OUT_PARA $TPU_Parameter \
+		--batch_size=$BATCH_SIZE
 
 else
-    python run_classifier.py \
-        --task_name=XNLI \
-        --do_train=true \
-        --do_eval=true \
-        --data_dir="$XNLI_DIR" \
-        --vocab_file="$BERT_BASE_DIR/vocab.txt" \
-        --bert_config_file="$BERT_BASE_DIR/bert_config.json" \
-        --init_checkpoint="$BERT_BASE_DIR/bert_model.ckpt" \
-        --do_lower_case=False \
-        --max_seq_length=128 \
-        --train_batch_size=32 \
-        --learning_rate=5e-5 \
-        --num_train_epochs=0.5 \
-        --output_dir=/tmp/xnli_output/
+	python run_classifier.py \
+		--task_name=XNLI \
+		--do_train=true \
+		--do_eval=true \
+		--data_dir="$XNLI_DIR" \
+		--vocab_file="$BERT_BASE_DIR/vocab.txt" \
+		--bert_config_file="$BERT_BASE_DIR/bert_config.json" \
+		--init_checkpoint="$BERT_BASE_DIR/bert_model.ckpt" \
+		--do_lower_case=False \
+		--max_seq_length=128 \
+		--train_batch_size=32 \
+		--learning_rate=5e-5 \
+		--num_train_epochs=0.5 \
+		--output_dir=/tmp/xnli_output/
 fi
