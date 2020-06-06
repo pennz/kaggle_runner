b6859364fc912e27f144a2424f96738ba8d36574
Vincent Zhou <fireflysuccess@gmail.com>
2020-06-06 08:14:52 +0800

    apex install
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- multilang_bert_data.sh
+++ multilang_bert_data.sh
@@ -13,8 +13,10 @@ git clone https://github.com/pennz/bert
 case $STAGE in
 pretrain)
     pip show apex || (
-        git clone --depth=1 https://github.com/NVIDIA/apex
-        pushd apex && python setup.py install --cuda_ext --cpp_ext && python -m pip install . && popd
+        [ -d /kaggle/input/apex-compiled-for-gpu-kernel/apex ] && (cp -rf /kaggle/input/apex-compiled-for-gpu-kernel/apex . && pushd apex && pip install .) || (
+            git clone --depth=1 https://github.com/NVIDIA/apex
+            pushd apex && python setup.py install --cuda_ext --cpp_ext && python -m pip install . && popd
+        )
     )
 
     python3 -c "from kaggle_runner.kernels.bert_torch import for_pytorch;from kaggle_runner.datasets.bert import pack_data; for_pytorch(pack_data());"
