0cb3a0139c166e77088e268cbb0dff391e0613a0
Zhou Pengyu <zhoupengyu@technomous.com.cn>
2020-06-18 21:47:26 +0800

    Update debug code
    Signed-off-by: Zhou Pengyu <zhoupengyu@technomous.com.cn>

--- hub/shonenkov_training_pipeline.py
+++ hub/shonenkov_training_pipeline.py
@@ -8,7 +8,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.5.0
+#       jupytext_version: 1.4.2
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
