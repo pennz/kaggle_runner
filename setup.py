5d1bdc5aa5b8537caec32bdead535cba428bd026
Vincent Zhou <fireflysuccess@gmail.com>
2020-07-15 13:34:06 +0800

    setup.py: v0.2.1
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kaggle-runner",
-    version="0.2.0",
+    version="0.2.1",
     description="Run kaggle kernels, for fast model prototyping.",
     url="http://github.com/pennz/kaggle_runner",
     author="pennz",
